64111c1cb65decdcf3d1a4cee1f6d3ef14844f70
Radmyr Deriabin <radmyr.deriabin11@gmail.com>
2022-08-23 14:00:42 +0200

    dz1

---
+++ dz1/script.js
@@ -0,0 +1,7 @@
+"use strict";
+let val1 = prompt ("Enter first value");
+val1 = Number(val1);
+let val2 = prompt ("Enter second value");
+val2 = Number(val2);
+const sum = val1 + val2;
+alert (val1 + "+" + val2 + "=" + sum);
