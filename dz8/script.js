c273695503cb42f142f74abe3586d57198a19d0d
Radmyr Deriabin <radmyr.deriabin11@gmail.com>
2022-09-22 05:00:19 +0200

    dz8

---
+++ dz8/script.js
@@ -0,0 +1,29 @@
+   
+function calculateResult(a,b,action,) {
+    switch (action) {
+        case '+':
+            return a + b;
+        case '-':
+            return a - b;
+        case '*':
+            return a * b;
+        case '/':
+            return a / b;
+    }
+} 
+calculate.onclick = function() {
+    let action = document.getElementById("operator").value;
+    let a = Number(document.getElementById('first__value').value);
+    let b = Number(document.getElementById('second__value').value);
+    let result = calculateResult(a,b,action)
+
+
+    return document.getElementById('str').innerHTML=`Exp: ${a} ${action} ${b} = ${result}`;
+}
+
+
+
+
+
+
+
