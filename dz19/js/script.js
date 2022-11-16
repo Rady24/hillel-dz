679b40de0af111737b9da477474465568ed7d761
Radmyr Deriabin <radmyr.deriabin11@gmail.com>
2022-11-16 22:23:32 +0100

    dz19

---
+++ dz19/js/script.js
@@ -0,0 +1,55 @@
+const API_URL = 'https://jsonplaceholder.typicode.com/photos?_limit=10';
+const itemTemplate = `<a href="" 
+class="example" 
+data-rbox-image="{{url}}" 
+data-rbox-caption="{{title}}"
+data-rbox-series="gallery">
+<img src="{{thumbnailUrl}}"  alt="{{title}}"/>
+</a> `;
+const containerEl = $('.container');
+let itemsList = [];
+
+init();
+initGallery();
+
+function initGallery(){
+    return $(".example").rbox({
+        'type': 'image',
+      })
+}
+
+
+
+function init() {
+    fetchItems();
+    renderList(itemsList);
+    
+}
+
+
+function fetchItems() {
+    fetch(API_URL)
+        .then((res) => res.json())
+        .then((data) => {
+            itemsList = data;
+            renderList(itemsList);
+        });
+}
+
+
+function renderList(list) {
+    
+    const html = list.map(generateItemHtml).join('');
+    return containerEl.append(html);
+}
+
+function generateItemHtml(item) {
+    return interpolate(itemTemplate, item)
+}
+
+
+
+
+
+
+
